6b4a4892f830d27f1306b16f06d61434c46ced1f
fastbyt3 <aldeerayan@gmail.com>
2023-10-29 19:31:39 +0530

    Handle concurrent connections

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,33 @@
-use std::{net::{TcpListener, TcpStream}, io::{Read, Write}};
+use std::{
+    io::{Read, Write},
+    net::{TcpListener, TcpStream},
+};
 
-fn handle_stream(mut stream: TcpStream) {
+async fn handle_stream(mut stream: TcpStream) {
     let mut buff = [0; 1024];
 
+    // Handle multiple PINGs from same conenction
     loop {
-        let bytes_recv = stream.read(&mut buff).expect("Unable to read stream into buffer");
+        let bytes_recv = stream.read(&mut buff).unwrap_or(0);
 
         if bytes_recv == 0 {
             break;
         }
 
-        let req = String::from_utf8(buff[..bytes_recv].to_vec()).expect("Error during parsing buffer as String");
-        println!("Request: {}", req);
+        let req = String::from_utf8(buff[..bytes_recv].to_vec())
+            .expect("Error during parsing buffer as String");
+        println!("Request: {:?}", req);
 
         let pong_resp = "+PONG\r\n";
-        stream.write_all(pong_resp.as_bytes()).expect("Write to TCP stream failed");
+        stream
+            .write_all(pong_resp.as_bytes())
+            .expect("Write to TCP stream failed");
     }
     stream.flush().unwrap();
 }
 
-fn main() {
+#[tokio::main]
+async fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
@@ -29,7 +37,9 @@ fn main() {
         match stream {
             Ok(stream) => {
                 println!("accepted new connection");
-                handle_stream(stream)
+                tokio::spawn(async move {
+                    handle_stream(stream).await;
+                });
             }
             Err(e) => {
                 println!("error: {}", e);
